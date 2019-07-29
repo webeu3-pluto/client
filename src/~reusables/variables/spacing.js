08a877df948431595319f0d80de2fb5cf285cae8
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-29 17:36:50 +0100

    Define project style variables

---
+++ src/~reusables/variables/spacing.js
@@ -0,0 +1,8 @@
+//====== SPACING VARIABLES ======//
+export const xs_space = "8px";
+export const small_space = "16px";
+export const medium_space_1 = "24px";
+export const medium_space_2 = "32px";
+export const medium_space_3 = "40px";
+export const large_space = "56px";
+export const xl_space = "80px";
