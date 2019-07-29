0e006231448feefa12008da62bbed7c9b802a45a
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-29 20:03:06 +0100

    Fix centering of landing components

--- src/components/Marketing/Landing/LandingHero.js
+++ src/components/Marketing/Landing/LandingHero.js
@@ -18,57 +18,60 @@ import { Link } from "react-router-dom";
 const LandingHero = () => {
   return (
     <StyledHero>
-      <div className="left-hero">
-        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
-          </p>
-          <Link to="/signup">
-            <ButtonPrimary>Sign up</ButtonPrimary>
-          </Link>
+      <div className="wrapper">
+        <div className="left-hero">
+          <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+            </p>
+            <Link to="/signup">
+              <ButtonPrimary>Sign up</ButtonPrimary>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="right-hero">
-        <img src={heroImage} alt="Web application dashboard interface" />
+        <div className="right-hero">
+          <img src={heroImage} alt="Web application dashboard interface" />
+        </div>
       </div>
     </StyledHero>
   );
 };
 
 const StyledHero = styled.section`
-  max-width: 1280px;
-  padding: 0 ${medium_space_3};
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  margin: ${large_space} 0;
+  .wrapper {
+    align-items: center;
+    margin: ${large_space} auto;
+    display: flex;
+    padding: 0 ${medium_space_3};
+    max-width: 1280px;
 
-  h1 {
-    font-size: ${heading_1};
-    color: ${headings};
-  }
-
-  p {
-    font-size: ${body_1};
-    color: ${text};
-  }
-
-  .left-hero {
-    width: 45%;
-    margin-right: 5%;
-
-    div {
-      display: flex;
-      align-items: center;
+    h1 {
+      font-size: ${heading_1};
+      color: ${headings};
     }
-  }
 
-  .right-hero {
-    width: 50%;
+    p {
+      font-size: ${body_1};
+      color: ${text};
+    }
 
-    img {
-      width: 100%;
+    .left-hero {
+      width: 45%;
+      margin-right: 5%;
+
+      div {
+        display: flex;
+        align-items: center;
+      }
+    }
+
+    .right-hero {
+      width: 50%;
+
+      img {
+        width: 100%;
+      }
     }
   }
 `;
--- src/components/Marketing/Landing/LandingMain.js
+++ src/components/Marketing/Landing/LandingMain.js
@@ -4,28 +4,37 @@ import styled from "styled-components";
 
 // components/functions
 import { light_grey } from "../../../~reusables/variables/colors";
-import { medium_space_3, xl_space } from "../../../~reusables/variables/spacing";
+import {
+  medium_space_3,
+  xl_space
+} from "../../../~reusables/variables/spacing";
 
 // styles
 
 const LandingMain = () => {
-  return (<StyledMain>
-  <div className="left-main">
-    <h3>Lorem ipsum dolor sit amet</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. Lorem ipsum dolor sit amet, consectetur.</p>
-  </div>
-  <div className="right-main">
-
-  </div>
-
-  </StyledMain>);
+  return (
+    <StyledMain>
+      <div className="wrapper">
+        <div className="left-main">
+          <h3>Lorem ipsum dolor sit amet</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+            Lorem ipsum dolor sit amet, consectetur.
+          </p>
+        </div>
+        <div className="right-main" />
+      </div>
+    </StyledMain>
+  );
 };
 
 const StyledMain = styled.section`
-  max-width: 1280px;
-  padding: ${xl_space} ${medium_space_3};
-  margin: 0 auto;
   background: ${light_grey};
+  .wrapper {
+    max-width: 1280px;
+    padding: ${xl_space} ${medium_space_3};
+    margin: 0 auto;
+  }
 `;
 
 export default LandingMain;
