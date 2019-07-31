e91f52aa5f7309c115ede5a57f4c7b676047bd8f
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-31 06:36:54 +0100

    Add error handlers to auth reducer

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,16 +1,34 @@
 import axios from "axios";
 
 export const SIGN_UP = "SIGN_UP";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
 export const LOG_IN = "LOG_IN";
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
 export const VALIDATE_USER = "VALIDATE_USER";
 
 const server = "https://plutoserver.herokuapp.com";
 
 export const signUp = (user, history) => async dispatch => {
   const res = await axios.post(`${server}/api/auth/register`, user);
+  dispatch({ type: SIGN_UP })
 
   // to do - grab token
   if (res.status === 201) {
+    const user = {
+      firstName: "Isaac",
+      lastName: "Aderogba",
+      role: 'Team Lead',
+      cohort: 'WEBEU3',
+      email: "isaacaderogba1@gmail.com",
+    };
+
+    dispatch({ type: SIGN_UP_SUCCESS, payload: user })
+
+    localStorage.setItem("token", res.data.token);
+    localStorage.setItem("user", JSON.stringify(user))
+
     history.push("/app");
   }
 };
@@ -27,6 +45,7 @@ export const validateUser = () => dispatch => {
 
 export const logIn = (user, history) => async dispatch => {
   try {
+    dispatch({ type: LOG_IN })
     const res = await axios.post(`${server}/api/auth/login`, user);
 
     if (res.status === 200) {
@@ -38,7 +57,7 @@ export const logIn = (user, history) => async dispatch => {
         email: "isaacaderogba1@gmail.com",
       };
 
-      dispatch({ type: LOG_IN, payload: user })
+      dispatch({ type: LOG_IN_SUCCESS, payload: user })
 
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("user", JSON.stringify(user))
@@ -47,5 +66,7 @@ export const logIn = (user, history) => async dispatch => {
     }
   } catch (err) {
     console.log(err);
+    // dispatch({ type: LOG_IN_FAILURE, payload: res.data })
+
   }
 };
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,17 +1,61 @@
-import { SIGN_UP, LOG_IN, VALIDATE_USER } from "../actions/authActions";
+import * as types from "../actions/authActions";
 
 const initState = {
-  isSignedIn: null
+  isSignedIn: null,
+  authLoader: null,
+  authError: null
 };
 
 export default function(state = initState, action) {
   switch (action.type) {
-    case VALIDATE_USER:
-      return { ...state, user: action.payload.user, isSignedIn: action.payload.status };
-    case SIGN_UP:
-      return { ...state, isSignedIn: true, user: action.payload };
-    case LOG_IN:
-      return { ...state, isSignedIn: true, user: action.payload };
+    case types.VALIDATE_USER:
+      return {
+        ...state,
+        user: action.payload.user,
+        isSignedIn: action.payload.status
+      };
+    case types.SIGN_UP:
+      return {
+        ...state,
+        authLoader: true,
+        authError: null
+      };
+    case types.LOG_IN:
+      return {
+        ...state,
+        authLoader: true,
+        authError: null
+      };
+    case types.SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        isSignedIn: true,
+        user: action.payload,
+        authLoader: false,
+        authError: null
+      };
+    case types.LOG_IN_SUCCESS:
+      return {
+        ...state,
+        isSignedIn: true,
+        user: action.payload,
+        authLoader: false,
+        authError: null
+      };
+    case types.SIGN_UP_FAILURE:
+      return {
+        ...state,
+        isSignedIn: false,
+        authLoader: false,
+        authError: action.payload
+      };
+    case types.LOG_IN_FAILURE:
+      return {
+        ...state,
+        isSignedIn: false,
+        authLoader: false,
+        authError: action.payload
+      };
     default:
       return state;
   }
