9c8b31e7cd8999f1cb333e4b380acd02c7d168a2
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-08-02 06:21:22 +0100

    Add quiz-details component

--- src/components/App/CreateQuiz/CreateQuiz.js
+++ src/components/App/CreateQuiz/CreateQuiz.js
@@ -1,9 +1,72 @@
-import React from 'react';
+// modules
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import styled from "styled-components";
+import {
+  medium_space_2,
+  large_space,
+  small_space
+} from "../../../~reusables/variables/spacing";
+import QuizDetails from "./QuizDetails";
 
-const CreateQuiz = () => {
-  return (
-    <div>CreateQuiz</div>
-  )
+// components/functions
+
+// styles
+
+const CreateQuiz = props => {
+  const { user } = props;
+  if (user.role === "Student") {
+    return <Redirect to="/app" />;
+  } else {
+    return (
+      <StyledCreateQuiz>
+        <div className="quiz-header">
+          <div className="quiz-details">
+            <QuizDetails />
+          </div>
+          <div className="quiz-status">
+            <QuizDetails />
+          </div>
+        </div>
+        <div className="quiz-body" />
+      </StyledCreateQuiz>
+    );
+  }
+};
+
+const StyledCreateQuiz = styled.div`
+  margin: ${medium_space_2} ${large_space};
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+
+  .quiz-header {
+    display: flex;
+    justify-content: space-between;
+
+    div {
+      flex: 1 1 200px;
+    }
+
+    .quiz-details {
+      margin-right: ${small_space};
+    }
+
+    .quiz-status {
+      margin-left: ${small_space};
+    }
+  }
+
+  @media only screen and (max-width: 767px) {
+    margin: ${small_space};
+  }
+`;
+
+function mapStateToProps(state) {
+  return {
+    user: state.auth.user
+  };
 }
 
-export default CreateQuiz;
+export default connect(mapStateToProps)(CreateQuiz);
---
+++ src/components/App/CreateQuiz/QuizDetails.js
@@ -0,0 +1,40 @@
+// modules
+import React from "react";
+import styled from "styled-components";
+
+// components/functions
+import { LineInput } from "../../../~reusables/atoms/Inputs";
+
+// styles
+import { medium_space_2, medium_space_1 } from "../../../~reusables/variables/spacing";
+
+const QuizDetails = () => {
+  return (
+    <StyledQuizDetails>
+          <p className="label">First name</p>
+
+          <LineInput
+          // value={firstName}
+          placeholder="Your first name"
+          // onChange={e => setFirstName(e.target.value)}
+        />
+        <p className="label">Last name</p>
+        <LineInput
+          // value={lastName}
+          placeholder="Your last name"
+          // onChange={e => setLastName(e.target.value)}
+        />
+    </StyledQuizDetails>
+  );
+};
+
+const StyledQuizDetails = styled.div`
+    background: #ffffff;
+    box-shadow: 0px 3px 8px rgba(56, 105, 160, 0.25);
+    padding: ${medium_space_2} ${medium_space_1};
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export default QuizDetails;
