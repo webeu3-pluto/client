5d0759a2a9df14ed0e90d9f18479bf8b1f170fe5
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-08-02 09:51:38 +0100

    Add students people view

--- src/components/App/People/StudentsPPL.js
+++ src/components/App/People/StudentsPPL.js
@@ -1,33 +1,88 @@
 // modules
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { connect } from "react-redux";
 
 // components/functions
+import AddPeopleModal from "./AddPeopleModal";
 import { ButtonTertiary } from "../../../~reusables/atoms/Buttons";
+import {
+  getStudentsByCohort,
+  addStudentToPeople
+} from "../../../store/actions/peopleActions";
+import PeopleList from "../../../~reusables/molecules/PeopleList";
 
 // styles
 import {
   medium_space_2,
   large_space,
-  medium_space_3
+  medium_space_3,
+  small_space,
+  medium_space_1
 } from "../../../~reusables/variables/spacing";
 import { support } from "../../../~reusables/variables/colors";
 
-const Students = () => {
+const StudentsPPL = props => {
+  const [modal, setModal] = useState(false);
+  const { getStudentsByCohort, addStudentToPeople } = props;
+
+  const onAddStudent = () => {
+    getStudentsByCohort();
+    setModal(true);
+  };
+
+  const people = [
+    { name: "Isaac A", quizzes: "2", score: 85 },
+    { name: "Isaac Ad ", quizzes: "2", score: 85 },
+    { name: "Isaac Ade", quizzes: "2", score: 85 },
+    { name: "Isaac Ader ", quizzes: "2", score: 85 },
+    { name: "Isaac Adero", quizzes: "2", score: 85 },
+    { name: "Isaac Aderog ", quizzes: "2", score: 85 },
+    { name: "Isaac Aderogb", quizzes: "2", score: 85 },
+    { name: "Isaac Aderogba ", quizzes: "2", score: 85 },
+    { name: "Isaa Aderogba", quizzes: "2", score: 85 },
+    { name: "Isa Aderogba ", quizzes: "2", score: 85 },
+    { name: "Isaac A", quizzes: "2", score: 85 },
+    { name: "Isaac Ad ", quizzes: "2", score: 85 },
+    { name: "Isaac Ade", quizzes: "2", score: 85 },
+    { name: "Isaac Ader ", quizzes: "2", score: 85 },
+    { name: "Isaac Adero", quizzes: "2", score: 85 },
+    { name: "Isaac Aderog ", quizzes: "2", score: 85 },
+    { name: "Isaac Aderogb", quizzes: "2", score: 85 },
+    { name: "Isaac Aderogba ", quizzes: "2", score: 85 },
+    { name: "Isaa Aderogba", quizzes: "2", score: 85 },
+    { name: "Isa Aderogba ", quizzes: "2", score: 85 }
+  ];
+
   return (
-    <StyledStudent>
+    <StyledProfile>
       <div className="wrapper">
+        {modal && (
+          <AddPeopleModal
+            functionCb={addStudentToPeople}
+            closeModal={setModal}
+            heading="Lorem ipsum"
+            paragraph="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+          />
+        )}
         <div className="header">
           <h4>TEAM LEADS</h4>
-          <ButtonTertiary>Add Team Lead</ButtonTertiary>
+          <ButtonTertiary onClick={onAddStudent}>Add Team Lead</ButtonTertiary>
         </div>
-        <div className="body" />
+        <div className="body">
+          <PeopleList
+            firstHeading="Name"
+            secondHeading="Quizzes Complete"
+            thirdHeading="Avg. Score"
+            listOfPeople={people}
+          />
+        </div>
       </div>
-    </StyledStudent>
+    </StyledProfile>
   );
 };
 
-const StyledStudent = styled.main`
+const StyledProfile = styled.main`
   margin: ${medium_space_2} ${large_space};
 
   background: #ffffff;
@@ -35,7 +90,8 @@ const StyledStudent = styled.main`
   border-radius: 8px;
 
   .wrapper {
-    padding: ${medium_space_3} ${medium_space_2};
+    padding: ${medium_space_3} ${medium_space_2} ${medium_space_1}
+      ${medium_space_2};
     display: flex;
     flex-direction: column;
   }
@@ -50,6 +106,24 @@ const StyledStudent = styled.main`
       letter-spacing: 0.25rem;
     }
   }
+
+  @media only screen and (max-width: 767px) {
+    margin: ${small_space};
+
+    .wrapper {
+      padding: ${medium_space_1} ${small_space};
+    }
+
+    .header {
+      h4 {
+        font-size: 14px;
+        letter-spacing: 0.2rem;
+      }
+    }
+  }
 `;
 
-export default Students;
+export default connect(
+  null,
+  { getStudentsByCohort, addStudentToPeople }
+)(StudentsPPL);
